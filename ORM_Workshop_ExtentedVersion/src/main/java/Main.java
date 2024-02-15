2b4b1bf06f74339e5f83f646c067696a3cf6400b
baldzhiyski__ <hristo_baldjiiski04@abv.bg>
2024-02-15 18:45:46 +0200

    Full Custom ORM

--- ORM_Workshop_ExtentedVersion/src/main/java/Main.java
+++ ORM_Workshop_ExtentedVersion/src/main/java/Main.java
@@ -1,5 +1,25 @@
+import entities.Account;
+import entities.User;
+import orm.EntityManager;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws SQLException, IllegalAccessException {
+            final EntityManager<User> userEntityManager = new EntityManager<>();
+            final EntityManager<Account> accountEntityManager = new EntityManager<>();
+
+            final User deleteTest = User.builder()
+                    .id(1)
+                    .username("Nikolay Markov")
+                    .age(51)
+                    .build();
+
+//        accountEntityManager.doCreate(Account.class);
+//        accountEntityManager.doAlter(Account.class);
+            userEntityManager.doDelete(deleteTest);
+
+            System.out.println();
+
     }
 }
