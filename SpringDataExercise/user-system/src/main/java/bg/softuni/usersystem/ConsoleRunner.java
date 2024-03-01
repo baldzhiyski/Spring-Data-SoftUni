e8a8a61e636fbf72c508cb4b32df12c84ef5789a
baldzhiyski__ <hristo_baldjiiski04@abv.bg>
2024-03-01 10:39:13 +0200

    Adding main method

--- SpringDataExercise/user-system/src/main/java/bg/softuni/usersystem/ConsoleRunner.java
+++ SpringDataExercise/user-system/src/main/java/bg/softuni/usersystem/ConsoleRunner.java
@@ -1,13 +1,52 @@
 package bg.softuni.usersystem;
 
 
+import bg.softuni.usersystem.domain.entities.User;
+import bg.softuni.usersystem.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 @Component
 public class ConsoleRunner implements CommandLineRunner {
+    private UserService userService;
+
+    @Autowired
+    public ConsoleRunner(UserService userService) {
+        this.userService = userService;
+    }
+
     @Override
     public void run(String... args) throws Exception {
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter 1 for seeing emails . Enter 2 for updating users.");
+        int number = scanner.nextInt();
+
+        switch (number){
+            case 1:
+                System.out.println("Give a provider : ");
+                String provider = scanner.nextLine();
+
+                printAllEmails(provider);
+                break;
+            case 2:
+                System.out.println("Give a date : ");
+                LocalDate date = LocalDate.parse(scanner.nextLine());
+                this.userService.setFieldIsDeletedTrueAllUsersNotLoggedAfterDate(date);
+                break;
+            default:throw new RuntimeException("Invalid input");
+        }
+
+    }
+
+    private void printAllEmails(String provider) {
+        this.userService.getUsersByEmailProvider(provider)
+                .stream().map(User::getEmail)
+                .forEach(System.out::println);
     }
 }
