db5d5f8864e0eb50d8b65385cd39a9e94fff93bc
baldzhiyski__ <hristo_baldjiiski04@abv.bg>
2024-03-11 19:44:06 +0200

    Creating the Paths and Utils class

---
+++ JSON-Exercise-SecondTask/src/main/java/bg/softuni/jsonexercisesecondtask/constants/Utils.java
@@ -0,0 +1,24 @@
+package bg.softuni.jsonexercisesecondtask.constants;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+
+public enum Utils {
+    ;
+
+    public static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
+    public static <T> void writeJsonOnFile(List<T> objects, Path path) throws IOException {
+        final FileWriter fileWriter = new FileWriter(String.valueOf(path));
+
+        GSON.toJson(objects, fileWriter);
+
+        fileWriter.flush();
+        fileWriter.close();
+    }
+}
