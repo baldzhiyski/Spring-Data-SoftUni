d35fffc1d21877c106681c14a5550f5327582063
baldzhiyski__ <hristo_baldjiiski04@abv.bg>
2024-03-11 16:06:10 +0200

    Creating the structure of the new task

---
+++ JSON-Exercise-SecondTask/src/test/java/bg/softuni/jsonexercisesecondtask/JsonExerciseSecondTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.jsonexercisesecondtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonExerciseSecondTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
