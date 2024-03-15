46ca9b7d06699bf812819339bc4f5ae8c75fb592
baldzhiyski__ <hristo_baldjiiski04@abv.bg>
2024-03-15 08:44:42 +0200

    Changing paths

--- JSON-Exercise-SecondTask/src/main/java/bg/softuni/jsonexercisesecondtask/constants/Paths.java
+++ JSON-Exercise-SecondTask/src/main/java/bg/softuni/jsonexercisesecondtask/constants/Paths.java
@@ -2,14 +2,14 @@ package bg.softuni.jsonexercisesecondtask.constants;
 
 public enum Paths {
     ;
-    public static final String PATH_TO_SUPPLIERS_INPUT ="C:\\Users\\user\\IdeaProjects\\Spring Data SoftUni\\JSON-Exercise-SecondTask\\src\\main\\resources\\input\\suppliers.json" ;
-    public static final String PATH_TO_PARTS_INPUT ="C:\\Users\\user\\IdeaProjects\\Spring Data SoftUni\\JSON-Exercise-SecondTask\\src\\main\\resources\\input\\parts.json" ;
-    public static final String PATH_TO_CARS_INPUT ="C:\\Users\\user\\IdeaProjects\\Spring Data SoftUni\\JSON-Exercise-SecondTask\\src\\main\\resources\\input\\cars.json" ;
-    public static final String PATH_TO_CUSTOMERS_INPUT ="C:\\Users\\user\\IdeaProjects\\Spring Data SoftUni\\JSON-Exercise-SecondTask\\src\\main\\resources\\input\\customers.json" ;
-    public static final String PATH_FIRST_EX ="C:\\Users\\user\\IdeaProjects\\Spring Data SoftUni\\JSON-Exercise-SecondTask\\src\\main\\resources\\outputs(solutions)\\ordered-customer.json" ;
-    public static final String PATH_SECOND_EX ="C:\\Users\\user\\IdeaProjects\\Spring Data SoftUni\\JSON-Exercise-SecondTask\\src\\main\\resources\\outputs(solutions)\\toyota-cars.json" ;
-    public static final String PATH_THIRD_EX ="C:\\Users\\user\\IdeaProjects\\Spring Data SoftUni\\JSON-Exercise-SecondTask\\src\\main\\resources\\outputs(solutions)\\local-suppliers.json" ;
-    public static final String PATH_FORTH_EX ="C:\\Users\\user\\IdeaProjects\\Spring Data SoftUni\\JSON-Exercise-SecondTask\\src\\main\\resources\\outputs(solutions)\\cars-and-parts.json" ;
-    public static final String PATH_FIFTH_EX ="C:\\Users\\user\\IdeaProjects\\Spring Data SoftUni\\JSON-Exercise-SecondTask\\src\\main\\resources\\outputs(solutions)\\customers-total-sales.json" ;
-    public static final String PATH_SIXTH_EX ="C:\\Users\\user\\IdeaProjects\\Spring Data SoftUni\\JSON-Exercise-SecondTask\\src\\main\\resources\\outputs(solutions)\\sales-discounts.json" ;
+    public static final String PATH_TO_SUPPLIERS_INPUT ="C:\\Users\\user\\IdeaProjects\\Spring-Data-SoftUni\\JSON-Exercise-SecondTask\\src\\main\\resources\\input\\suppliers.json" ;
+    public static final String PATH_TO_PARTS_INPUT ="C:\\Users\\user\\IdeaProjects\\Spring-Data-SoftUni\\JSON-Exercise-SecondTask\\src\\main\\resources\\input\\parts.json" ;
+    public static final String PATH_TO_CARS_INPUT ="C:\\Users\\user\\IdeaProjects\\Spring-Data-SoftUni\\JSON-Exercise-SecondTask\\src\\main\\resources\\input\\cars.json" ;
+    public static final String PATH_TO_CUSTOMERS_INPUT ="C:\\Users\\user\\IdeaProjects\\Spring-Data-SoftUni\\JSON-Exercise-SecondTask\\src\\main\\resources\\input\\customers.json" ;
+    public static final String PATH_FIRST_EX ="C:\\Users\\user\\IdeaProjects\\Spring-Data-SoftUni\\JSON-Exercise-SecondTask\\src\\main\\resources\\outputs(solutions)\\ordered-customer.json" ;
+    public static final String PATH_SECOND_EX ="C:\\Users\\user\\IdeaProjects\\Spring-Data-SoftUni\\JSON-Exercise-SecondTask\\src\\main\\resources\\outputs(solutions)\\toyota-cars.json" ;
+    public static final String PATH_THIRD_EX ="C:\\Users\\user\\IdeaProjects\\Spring-Data-SoftUni\\JSON-Exercise-SecondTask\\src\\main\\resources\\outputs(solutions)\\local-suppliers.json" ;
+    public static final String PATH_FORTH_EX ="C:\\Users\\user\\IdeaProjects\\Spring-Data-SoftUni\\JSON-Exercise-SecondTask\\src\\main\\resources\\outputs(solutions)\\cars-and-parts.json" ;
+    public static final String PATH_FIFTH_EX ="C:\\Users\\user\\IdeaProjects\\Spring-Data-SoftUni\\JSON-Exercise-SecondTask\\src\\main\\resources\\outputs(solutions)\\customers-total-sales.json" ;
+    public static final String PATH_SIXTH_EX ="C:\\Users\\user\\IdeaProjects\\Spring-Data-SoftUni\\JSON-Exercise-SecondTask\\src\\main\\resources\\outputs(solutions)\\sales-discounts.json" ;
 }
