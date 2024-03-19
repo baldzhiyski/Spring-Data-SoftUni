d6c2ee6112011898f51c3cb802e1213e2491f142
baldzhiyski__ <hristo_baldjiiski04@abv.bg>
2024-03-19 10:08:47 +0200

    Adding the configuration class, xmlParser and Validator

--- skeleton/src/main/java/softuni/exam/config/ApplicationBeanConfiguration.java
+++ skeleton/src/main/java/softuni/exam/config/ApplicationBeanConfiguration.java
@@ -1,6 +1,39 @@
 package softuni.exam.config;
 
-// TODO:
-public class ApplicationBeanConfiguration {
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.modelmapper.AbstractConverter;
+import org.modelmapper.Converter;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@Configuration
+public class ApplicationBeanConfiguration {
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.addConverter(stringToLocalDateConverter());
+        return modelMapper;
+    }
+
+    private Converter<String, LocalDate> stringToLocalDateConverter() {
+        return new AbstractConverter<String, LocalDate>() {
+            @Override
+            protected LocalDate convert(String source) {
+                // Define the format of your date string
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                // Parse the string to LocalDate using the defined format
+                return LocalDate.parse(source, formatter);
+            }
+        };
+    }
+
+    @Bean
+    public Gson gson() {
+        return new GsonBuilder().setPrettyPrinting().create();
+    }
 }
--- skeleton/src/main/java/softuni/exam/util/XmlParserImpl.java
+++ skeleton/src/main/java/softuni/exam/util/XmlParserImpl.java
@@ -2,6 +2,18 @@ package softuni.exam.util;
 
 import org.springframework.stereotype.Component;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
+
 @Component
 public class XmlParserImpl implements XmlParser{
+    @Override
+    public <T> T xmlParse(String filePath, Class<T> tClass) throws JAXBException {
+        JAXBContext jaxbContext = JAXBContext.newInstance(tClass);
+        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+
+        return (T) unmarshaller.unmarshal(new File(filePath));
+    }
 }
