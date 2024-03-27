c66f472a697b2260cfac9494b8f76fbb320c979b
baldzhiyski__ <hristo_baldjiiski04@abv.bg>
2024-03-27 10:12:58 +0200

    Importing the skeleton

---
+++ Job-Finder-Exam/src/main/java/softuni/exam/util/XmlParser.java
@@ -0,0 +1,16 @@
+package softuni.exam.util;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
+
+public class XmlParser {
+
+    public <T> T xmlParse(String filePath, Class<T> tClass) throws JAXBException {
+        JAXBContext jaxbContext = JAXBContext.newInstance(tClass);
+        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+
+        return (T) unmarshaller.unmarshal(new File(filePath));
+    }
+}
